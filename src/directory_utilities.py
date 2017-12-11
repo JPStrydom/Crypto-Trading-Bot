9a96cbad9d814e3fa309d8383e47a2f99cf45709
JP Strydom <jpstrydom8@gmail.com>
2017-12-11 18:46:56 +0200

    Add descriptions to directory utilities

--- src/directory_utilities.py
+++ src/directory_utilities.py
@@ -4,6 +4,12 @@ import json
 
 
 def validate_or_make_directory(directory_string):
+    """
+    Check if a directory exists. If it doesn't, then create it.
+
+    :param directory_string: The relative directory string (ex: database/secrets.json)
+    :type directory_string: str
+    """
     if not os.path.exists(os.path.dirname(directory_string)):
         try:
             os.makedirs(os.path.dirname(directory_string))
@@ -13,7 +19,16 @@ def validate_or_make_directory(directory_string):
                 raise
 
 
-def get_json_from_file(directory_string, default_json_content):
+def get_json_from_file(directory_string, default_json_content=None):
+    """
+    Get the contents of a JSON file. If it doesn't exist,
+    create and populate it with specified or default JSON content.
+
+    :param directory_string: The relative directory string (ex: database/secrets.json)
+    :type directory_string: str
+    :param default_json_content: The content to populate a non-existing JSON file with
+    :type default_json_content: dict
+    """
     validate_or_make_directory(directory_string)
     try:
         with open(directory_string) as file:
@@ -22,12 +37,23 @@ def get_json_from_file(directory_string, default_json_content):
             return file_content
     except (IOError, json.decoder.JSONDecodeError):
         with open(directory_string, 'w') as file:
+            if default_json_content is None:
+                default_json_content = {}
             json.dump(default_json_content, file, indent=4)
             file.close()
             return default_json_content
 
 
 def write_json_to_file(directory_string, json_content):
+    """
+    Get the contents of a JSON file. If it doesn't exist,
+    create and populate it with specified or default JSON content.
+
+    :param directory_string: The relative directory string (ex: database/secrets.json)
+    :type directory_string: str
+    :param json_content: The content to populate a non-existing JSON file with
+    :type json_content: dict
+    """
     with open(directory_string, 'w') as file:
         json.dump(json_content, file, indent=4)
         file.close()
